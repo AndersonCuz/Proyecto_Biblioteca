55c75d4442532a382d9c481286fd6491e2a8e1fc
andersoncuz <rodrigocuz115@gmail.com>
2022-10-18 20:27:13 -0600

    Proyecto Subido

---
+++ app/Views/cargarUsuario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/estilo2.css">
+    <title>INICIO DE SESION</title>
+</head>
+
+<body>
+    <h1 align="center">BIENVENIDO INICIE SESION</h1>
+    <div class="center">
+        <h1>REGISTRARSE</h1>
+        <form method="post">
+            <div class="txt_field">
+                <input type="text" required>
+                <span></span>
+                <label>Nombre Usuario</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" required>
+                <span></span>
+                <label>Contraseña</label>
+            </div>
+            <input type="submit" value="INICIAR">
+            <hr>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ app/Views/iniciomenu.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesion</title>
+    <link rel="stylesheet" href="../CSS/estilo.css">
+</head>
+<body>
+<div class="contenedor"> 
+        <div class="item1">
+            <p><img src="../imagenes/banner.png" alt="IMAGEN NO ENCONTRADA" width=""></p>
+        </div>
+        <div class="item2">
+            <a href="<?= base_url('cargar_usuario')?>" style="text-decoration:none;" target="parent">
+                <div align="center" class="formulario">
+                    <div class="carreaux_presentation_light" style="background-image:url(../imagenes/administrador.png);">
+                        <div class="shadow_swhow_mini">
+                            <div class="deroul_titre">USUARIO Administrador</div>
+                            <div class="deroul_soustitre">Biblioteca</div>
+                        </div>
+                    </div>
+            
+                    <div style=" padding:5px; color:#1e4944; font-weight:300; font-size:30px; font-family:'Roboto';padding-top:20px;">
+                        USUARIO <font style="font-weight:400;">Administrador</font>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="item3">
+            <a href="<?= base_url('cargar_usuario')?>" style="text-decoration:none;" target="parent">
+                <div align="center" class="formulario">
+                    <div class="carreaux_presentation_light" style="background-image:url(../imagenes/bibliotecaria2.png);">
+                        <div class="shadow_swhow_mini">
+                            <div class="deroul_titre">USUARIO Bibliotecaria</div>
+                            <div class="deroul_soustitre">Biblioteca</div>
+                        </div>
+                    </div>
+                    <div style=" padding:5px; color:#1e4944; font-weight:300; font-size:30px; font-family:'Roboto';padding-top:20px;">
+                        USUARIO <font style="font-weight:400;">Bibliotecaria</font>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="item4">
+            <a href="<?= base_url('cargar_usuario')?>" style="text-decoration:none;" target="parent"> 
+                <div align="center" class="formulario">
+                    <div class="carreaux_presentation_light" style="background-image:url(../imagenes/bibliotecaria.png);">
+                        <div class="shadow_swhow_mini">
+                            <div class="deroul_titre">USUARIO Estudiante</div>
+                            <div class="deroul_soustitre">Biblioteca</div>
+                        </div>
+                    </div>
+                    <div style=" padding:5px; color:#1e4944; font-weight:300; font-size:30px; font-family:'Roboto';padding-top:20px;">
+                         USUARIO <font style="font-weight:400;">Estudiante</font>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
